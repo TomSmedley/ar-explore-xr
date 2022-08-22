55f7f7d63237094c3eaddae105a5a96d174c3aab
Tom Smedley <hi@tomsmedley.co.uk>
2022-08-22 14:53:35 +0100

    Change to React-XR

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from 'react'
+import { ARCanvas } from '@react-three/xr'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 function App() {
+  const gltf = useLoader(GLTFLoader, '/room-1.gltf')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ARCanvas>
+      <Suspense fallback={null}>
+        <primitive object={gltf.scene} />
+      </Suspense>
+    </ARCanvas>
+  )
 }
 
-export default App;
+export default App
